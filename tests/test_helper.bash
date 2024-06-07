0f2189ec8031d45623b7b7f6661bbcb45f35bd8a
Christian Höltje <docwhat@gerf.org>
2024-06-07 02:40:13 -0400

    rewrote pseudo-tty to be more reliable and simpler

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -16,40 +16,5 @@ load-bashable() {
 }
 
 fake-tty() {
-  local tmp err cmd
-
-  # Create a temporary file for storing the status code
-  tmp=$(mktemp) || return 99
-
-  # Ensure it worked or fail with status 99
-  if [[ -z ${tmp} ]]; then
-    return 99
-  fi
-
-  # Produce a script that runs the command provided to faketty as
-  # arguments and stores the status code in the temporary file
-  cmd="$(printf '%q ' "$@"); echo \$? > $(printf '%q' "${tmp}")"
-
-  # Run the script through /bin/sh with fake tty
-  if [[ "$(uname -s || :)" == "Darwin" ]]; then
-    # MacOS
-    script -Fq /dev/null /bin/sh -c "${cmd}"
-  else
-    script -qfc "/bin/sh -c $(printf "%q " "${cmd}")" /dev/null
-  fi
-
-  # Ensure that the status code was written to the temporary file or
-  # fail with status 99
-  if ! [[ -s ${tmp} ]]; then
-    return 99
-  fi
-
-  # Collect the status code from the temporary file
-  err=$(cat "${tmp}")
-
-  # Remove the temporary file
-  rm -f "${tmp}"
-
-  # Return the status code
-  return "${err}"
+  "${BASHABLE_GIT_ROOT}/tests/test_helper/pseudo-tty" "$@"
 }
