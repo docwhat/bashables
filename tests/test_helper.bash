c94e1aac1fc4ac3df4a1cb55d48db7abb6b68cad
Christian Höltje <docwhat@gerf.org>
2024-06-08 16:51:42 -0400

    moved bats support libs in vendor directory

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -2,9 +2,9 @@
 # shellcheck disable=SC2034
 
 load-helpers() {
-  load test_helper/bats-assert-2.1.0/load
-  load test_helper/bats-file-0.4.0/load
-  load test_helper/bats-support-0.3.0/load
+  load "${BASHABLE_GIT_ROOT}/vendor/bats-assert-2.1.0/load"
+  load "${BASHABLE_GIT_ROOT}/vendor/bats-file-0.4.0/load"
+  load "${BASHABLE_GIT_ROOT}/vendor/bats-support-0.3.0/load"
 }
 
 BASHABLE_GIT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
